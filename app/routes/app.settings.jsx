0ef6cca217d10dc34df9f7b7eea004073ae2e6af
Shah Shah <shahidshah.coderkube@gmail.com>
2026-06-10 18:29:38 +0530

    feat: replace template additional page with a dedicated settings page

---
+++ app/routes/app.settings.jsx
@@ -0,0 +1,11 @@
+export default function SettingsPage() {
+  return (
+    <s-page heading="Settings">
+      <s-section heading="App Settings">
+        <s-paragraph>
+          This is the settings page. Please let us know what features or settings you want to place here, and we will build them for you!
+        </s-paragraph>
+      </s-section>
+    </s-page>
+  );
+}
